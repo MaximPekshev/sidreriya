622c565c2d7cd0998f82e3341d55abb3704fbde1
MaximPekshev <mp198769@gmail.com>
2020-04-23 18:26:57 +0300

    commit before deploy

--- sidreriya/settings.py
+++ sidreriya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o-w+*8qks^v)0%^r(hxk4-##)9psib9$y#n5f(wcql4yq@*i62'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',]
 
 
 # Application definition
@@ -128,8 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_main')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
