2f639d9bfefa3931292a81dcce61decab2759e3b
MaximPekshev <mp198769@gmail.com>
2021-04-20 14:17:22 +0300

    correct urls.py

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 	path('lunch-set/', 		show_set_lunch, name='show_set_lunch'),
 	path('gift-boxes/', 	show_gift_boxes, name='show_gift_boxes'),
 	path('breakfasts/', 	show_breakfasts, name='show_breakfasts'),
-	path('сertificate/', 	show_сertificate, name='show_сertificate'),
+	path('certificate/', 	show_сertificate, name='show_сertificate'),
 
 
 			]
