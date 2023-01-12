4e9ac263ac416fb2e88f6b209bdd7972675d5251
MaximPekshev <mp198769@gmail.com>
2023-01-12 12:16:46 +0300

    12012023

--- cartapp/models.py
+++ cartapp/models.py
@@ -13,7 +13,8 @@ def cart_calculate_summ(cart):
 		summ_of_cart = 0
 
 		for item in cart_items:
-			summ_of_cart += item.summ
+			if item.good.quantity !=0:
+				summ_of_cart += item.summ
 
 		cart.summ = summ_of_cart
 		cart.save()
@@ -60,7 +61,8 @@ class Cart(models.Model):
 	def get_discount_summ(self):
 		summ = Decimal(0)
 		for item in Cart_Item.objects.filter(cart=self):
-			summ += item.quantity*item.get_discount_price()
+			if item.good.quantity != 0:
+				summ += item.quantity*item.get_discount_price()
 		return summ.quantize(Decimal("1"))
 
 	def __str__(self):
