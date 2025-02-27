c9d5b37dd3e4e3e3b657473dadcc21f0e44a836a
MaximPekshev <info@annasoft.ru>
2025-02-27 07:15:05 +0300

    host settings

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/var/www/u0872810/data/www/sidreriaybelgorod.ru/sidreriya')
-sys.path.insert(1, '/var/www/u0872810/data/sidreriya/lib/python3.7/site-packages')
+sys.path.insert(0, '/var/www/u0872810/data/www/sidreriyabelgorod.ru/sidreriya')
+sys.path.insert(1, '/var/www/u0872810/data/www/sidreriyabelgorod.ru/.venv/lib/python3.10/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'sidreriya.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
